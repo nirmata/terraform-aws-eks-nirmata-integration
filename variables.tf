f4ab132f1321f20104e3730d02d69ff93b51488f
Anujramola1999 <anuj.ramola@nirmata.com>
2025-05-20 16:29:26 +0530

    updated variable.tf

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 variable "cluster_name" {
   description = "Name of the EKS cluster"
   type        = string
-  default     = "eks-cluster"
+  default     = "nvts-eks-cluster"
 }
 
 variable "cluster_version" {
